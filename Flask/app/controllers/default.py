a9607002a85e021a9ad0a0672dae3b617a1fc482
BielLopes <gabriel.galoma@gmail.com>
2018-11-27 18:38:30 -0200

    versão  funcional e login planejado

--- Flask/app/controllers/default.py
+++ Flask/app/controllers/default.py
@@ -1,12 +1,43 @@
 from app import app
+from flask import render_template
+
+from app.models.forms import loginFrom
 
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route("/index/<user>")
+@app.route("/", defaults={"user":None})
+def index(user):
+    return render_template('index.html',
+                            user=user)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form_login = loginFrom()
+    if form_login.validate_on_submit():
+        print(form_login.pasword.data)
+        print(form_login.username.data)
+    else:
+        print(form_login.errors)
+    return render_template('login.html',
+                            form_login=form_login)
+'''
+    """
+    <html>
+        <head>
+            <title>Hello World!</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+            <h2>Isto é uma string em block</h2>
+        </body>
+    </html>
+    """
+
+'''
 
 
+
+'''
 @app.route("/teste/<name>")
 @app.route("/teste")
 def teste(name=None):
@@ -16,12 +47,12 @@ def teste(name=None):
         return "vai embora"
 
 
-'''Utilizando os métodos POST e GET (é possivel filtrar de qual tipo será usado)'''
+Utilizando os métodos POST e GET (é possivel filtrar de qual tipo será usado)
 @app.route("/teste2/", methods=['GET'])
 def teste2():
     return "oi"
 
-'''
+
 Comessando a entender tipos na definiçãod de rotas
 @app.route("/teste/<name>")
 def teste(name=None):
